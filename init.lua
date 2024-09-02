e2452e2e335fcc76bd261b4992fbd1d7d0310de4
wingyou <winwing.you@gmail.com>
2024-09-03 00:36:51 +0900

    feat: extra keymaps and autocmds

--- init.lua
+++ init.lua
@@ -58,7 +58,7 @@ vim.opt.splitright=true
 vim.opt.startofline=true
 
 -- Set colorscheme.
-vim.cmd 'colorscheme retrobox'
+-- vim.cmd 'colorscheme retrobox'
 
 -- Enable matchit.
 vim.cmd 'packadd! matchit'
@@ -66,9 +66,12 @@ vim.cmd 'packadd! matchit'
 -- Set nohlsearch on pressing <ESC>.
 vim.keymap.set('n', '<ESC>', '<cmd>nohlsearch<CR>')
 
--- Resize window size with +, -.
-vim.keymap.set('n', '+', '<C-w>+', { desc = 'Increase current window size' })
-vim.keymap.set('n', '-', '<C-w>-', { desc = 'Decrease current window size' })
+-- Resize window size with +, _.
+-- NOTE: I used '_' instead of '-' because it's much easier to press '_' and '+' than '-' alternativly.
+vim.keymap.set('n', '+', '<C-w>+', { desc = 'Increase current window height' })
+vim.keymap.set('n', '_', '<C-w>-', { desc = 'Decrease current window height' })
+vim.keymap.set('n', '<C-+>', '<C-w>>', { desc = 'Increase current window width' })
+vim.keymap.set('n', '<C-_>', '<C-w><', { desc = 'Decrease current window width' })
 
 -- Move between windows with h, j, k, l.
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to left window' })
@@ -76,11 +79,33 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to lower window'
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to upper window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to right window' })
 
+-- Shortcut to execute external program
+vim.keymap.set('n', '!', ':!', { desc = 'Execute external program shortcut' })
+
+-- Press 'R' to substitute word under cursor.
+-- NOTE: 'R' has other default action. Since I don't use it, exchanged it.
+vim.keymap.set('n', 'R', function()
+  local word = vim.fn.expand('<cword>')
+  return ':%s/' .. word .. '/' 
+end, { expr = true, desc = 'Substitute word under cursor' })
+
 -- Highlight on yank.
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- Save file if modified before sourcing
+vim.api.nvim_create_autocmd('SourcePre', {
+  desc = 'Save file if modified before sourcing',
+  group = vim.api.nvim_create_augroup('write-before-souring', { clear = true }),
+  callback = function(ev)
+    local is_modified = vim.api.nvim_buf_get_option(ev.buf, 'modified')
+    if is_modified then
+      vim.cmd('write ' .. ev.file)
+    end
   end,
 })
