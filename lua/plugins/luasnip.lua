4f3367db9f78d20d11fd88ba05d34ad07a401207
wingyou <winwing.you@gmail.com>
2024-09-04 07:09:08 +0900

    feat: luasnip

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,29 @@
+return {
+  "L3MON4D3/LuaSnip",
+  -- follow latest release.
+  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  dependencies = {
+    "rafamadriz/friendly-snippets"
+  },
+  -- install jsregexp (optional!).
+  build = "make install_jsregexp",
+  config = function()
+    require'luasnip.loaders.from_vscode'.lazy_load()
+    local ls = require'luasnip'
+
+    -- FIXME: This code cannot pregress to the last position when continually jumping forward in insert mode.
+    -- Problem is fixed if I jump in select mode or returning from the end in select mode. Why?
+    -- vim.keymap.set({"i", "s"}, "<Tab>", function()
+      --   if ls.expand_or_jumpable() then
+      --     ls.jump(1)
+      --   else
+      --     return '<Tab>'
+      --   end
+      -- end, { silent = true, expr = true })
+      -- vim.keymap.set({"i", "s"}, "<S-Tab>", function() ls.jump(-1) end, {silent = true})
+
+      -- This keymap is pretty good. May I use this?
+      vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+      vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+  end
+}
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,12 +2,19 @@ return {
   'hrsh7th/nvim-cmp',
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
-    'hrsh7th/cmp-buffer'
+    'hrsh7th/cmp-buffer',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
   },
   config = function()
     local cmp = require'cmp'
 
     cmp.setup {
+      snippet = {
+        expand = function(args)
+          require'luasnip'.lsp_expand(args.body)
+        end
+      },
       mapping = cmp.mapping.preset.insert {
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
