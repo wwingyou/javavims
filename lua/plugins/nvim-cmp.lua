fe102f4bf05ff9cb45a324764c3845329761afe9
wingyou <winwing.you@gmail.com>
2024-09-04 15:34:00 +0900

    feat: nvim-cmp formatting

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,10 +5,11 @@ return {
     'hrsh7th/cmp-buffer',
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
+    'onsails/lspkind.nvim',
   },
   config = function()
     local cmp = require'cmp'
-
+    local lspkind = require'lspkind'
     cmp.setup {
       snippet = {
         expand = function(args)
@@ -16,8 +17,8 @@ return {
         end
       },
       mapping = cmp.mapping.preset.insert {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm(),
       },
@@ -25,6 +26,31 @@ return {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'buffer' },
+      },
+      window = {
+        completion = {
+          side_padding = 1,
+        },
+      },
+      formatting = {
+        expandable_indicator = true,
+        fields = { 'abbr', 'kind', 'menu' },
+        format = lspkind.cmp_format {
+          mode = 'symbol_text',
+          maxwidth = 30,
+          ellipsis_char = '...',
+          show_labelDetails = true,
+          menu = ({
+            buffer = "[Buffer]",
+            nvim_lsp = "[LSP]",
+            luasnip = "[LuaSnip]",
+          }),
+
+          -- NOTE: The function below will be called before any actual modifications from lspkind
+          before = function (entry, vim_item)
+            return vim_item
+          end
+        }
       }
     }
   end
