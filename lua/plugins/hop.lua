2a4d862aea14ba9fd8058303704d19e105971b0e
wingyou <winwing.you@gmail.com>
2024-09-04 16:14:53 +0900

    feat: new plugin 'hop.nvim'

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,22 @@
+return {
+  'phaazon/hop.nvim',
+  branch = 'v2', -- optional but strongly recommended
+  config = function()
+    -- place this in one of your configuration file(s)
+    local hop = require('hop')
+    hop.setup {}
+    local directions = require('hop.hint').HintDirection
+    vim.keymap.set('', 'f', function()
+      hop.hint_char1({ direction = directions.AFTER_CURSOR})
+    end, {remap=true})
+    vim.keymap.set('', 'F', function()
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR})
+    end, {remap=true})
+    vim.keymap.set('', 't', function()
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
+    end, {remap=true})
+    vim.keymap.set('', 'T', function()
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR, hint_offset = 1 })
+    end, {remap=true})
+  end
+}
