c4180c8b6cb53cb2b2625005a740b2309cb71d2c
wingyou <winwing.you@gmail.com>
2024-09-10 12:00:25 +0900

    feat: new plugin 'treesj'

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,12 @@
+return {
+  'Wansmer/treesj',
+  keys = { 'gj' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+  config = function()
+    require('treesj').setup {
+      use_default_keymaps = false
+    }
+
+    vim.keymap.set('n', 'gj', require'treesj'.toggle, { desc = 'Toggle treesj' })
+  end,
+}
