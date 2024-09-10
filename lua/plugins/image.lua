5e94b67692645481c48712901dddae11028aef59
wingyou <winwing.you@gmail.com>
2024-09-10 15:21:49 +0900

    chore: add some todo comments

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,36 +1,40 @@
 return {
-    "3rd/image.nvim",
-    config = function()
-        require("image").setup {
-            backend = "kitty",
-            kitty_method = "normal",
-            integrations = {
-                markdown = {
-                    enabled = true,
-                    clear_in_insert_mode = false,
-                    download_remote_images = true,
-                    only_render_image_at_cursor = true,
-                    filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-                    resolve_image_path = function (document_path, image_path, fallback)
-                        return fallback(document_path, image_path)
-                    end
-                },
-                html = {
-                    enabled = false,
-                },
-                css = {
-                    enabled = false,
-                },
-            },
-            max_width = nil,
-            max_height = nil,
-            max_width_window_percentage = nil,
-            max_height_window_percentage = 50,
-            window_overlap_clear_enabled = false,                                         -- toggles images when windows are overlapped
-            window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-            editor_only_render_when_focused = false,                                      -- auto show/hide images when the editor gains/looses focus
-            tmux_show_only_in_active_window = false,                                      -- auto show/hide images in the correct Tmux window (needs visual-activity off)
-            hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
-        }
-    end
+  "3rd/image.nvim",
+  -- NOTE: This plugin need many extra dependencies. 
+  -- See 'https://github.com/3rd/image.nvim?tab=readme-ov-file'.
+  -- Also, It has bug that image is preserved between tmux window switch. 
+  -- (https://github.com/3rd/image.nvim/issues/106) Hope this will be fixed soon.
+  -- TODO: It looks possible to implement image preview for oil and telescope upon this plugin.
+  -- try using api (https://github.com/3rd/image.nvim?tab=readme-ov-file#api)
+  opts = {
+    backend = "kitty",
+    kitty_method = "normal",
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = true,
+        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        resolve_image_path = function (document_path, image_path, fallback)
+          return fallback(document_path, image_path)
+        end
+      },
+      html = {
+        enabled = false,
+      },
+      css = {
+        enabled = false,
+      },
+    },
+    max_width = nil,
+    max_height = nil,
+    max_width_window_percentage = nil,
+    max_height_window_percentage = 50,
+    window_overlap_clear_enabled = false,                                         -- toggles images when windows are overlapped
+    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+    editor_only_render_when_focused = false,                                      -- auto show/hide images when the editor gains/looses focus
+    tmux_show_only_in_active_window = false,                                      -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+  }
 }
--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -2,22 +2,19 @@ return {
   "OXY2DEV/markview.nvim",
   lazy = false,      -- Recommended
   -- ft = "markdown" -- If you decide to lazy-load anyway
-
   dependencies = {
     -- You will not need this if you installed the
     -- parsers manually
     -- Or if the parsers are in your $RUNTIMEPATH
     "nvim-treesitter/nvim-treesitter",
-
     "nvim-tree/nvim-web-devicons"
   },
+  -- FIXME: Markdown icons in the informatic column has wrong background color.
   opts = {
     modes = { "n", "no", "c" }, -- Change these modes
     -- to what you need
-
     hybrid_modes = { "n" },     -- Uses this feature on
     -- normal mode
-
     -- This is nice to have
     callbacks = {
       on_enable = function (_, win)
