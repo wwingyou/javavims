d8e310ffc14fb1c628c57b58d1f8a5f60419d65f
wingyou <winwing.you@gmail.com>
2024-10-28 10:38:50 +0900

    feat(telescope): extend with fzf native extension
    Default telescope engine doesn't match white spaces. It was very
    unconvenient.
    
    Native fzf engine provide better fuzzy match performanc.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,16 @@ return {
       layout_strategy = 'vertical',
       layout_config = {
         vertical = { preview_height = 0.75 }
-      }
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,                    -- false will only do exact matching
+          override_generic_sorter = true,  -- override the generic sorter
+          override_file_sorter = true,     -- override the file sorter
+          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        }
+      },
     }
     require'telescope'.setup { defaults = default_opt }
 
