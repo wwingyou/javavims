e5f48cd05a86038e19e00e2dca7274828ee35848
wingyou <winwing.you@gmail.com>
2024-11-18 16:44:13 +0900

    feat(telescope): telescope select applied

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
     'nvim-treesitter/nvim-treesitter',
+    'nvim-telescope/telescope-ui-select.nvim',
   },
   config = function()
     local default_opt = {
@@ -23,7 +24,31 @@ return {
       },
       file_ignore_patterns = { "%.class" }
     }
-    require'telescope'.setup { defaults = default_opt }
+    require'telescope'.setup {
+      defaults = default_opt,
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+            -- even more opts
+          }
+
+          -- pseudo code / specification for writing custom displays, like the one
+          -- for "codeactions"
+          -- specific_opts = {
+          --   [kind] = {
+          --     make_indexed = function(items) -> indexed_items, width,
+          --     make_displayer = function(widths) -> displayer
+          --     make_display = function(displayer) -> function(e)
+          --     make_ordinal = function(e) -> string
+          --   },
+          --   -- for example to disable the custom builtin "codeactions" display
+          --      do the following
+          --   codeactions = false,
+          -- }
+        }
+      }
+    }
+    require'telescope'.load_extension('ui-select')
 
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', function() builtin.find_files(default_opt) end, {})
