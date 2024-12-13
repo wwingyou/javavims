e0996efaad855c253f83925182f3c6adbf808ea4
wingyou <winwing.you@gmail.com>
2024-12-14 02:26:33 +0900

    feat(lua): add lua execution keymaps

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -1,2 +1,7 @@
 vim.bo.tabstop=2
 vim.bo.shiftwidth=2
+
+-- Execute current lua script
+vim.keymap.set('n', '<leader>x', '<cmd>source %<CR>')      -- file
+vim.keymap.set('n', '<C-R>', ':.lua<CR>')                  -- current line
+vim.keymap.set('v', '<C-R>', ':lua<CR>')                   -- selected area
