1908e81270289024dbed08f4615db98d32412786
wingyou <winwing.you@gmail.com>
2024-12-17 01:29:17 +0900

    feat: change lua file runner keymap

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -2,6 +2,5 @@ vim.bo.tabstop=2
 vim.bo.shiftwidth=2
 
 -- Execute current lua script
-vim.keymap.set('n', '<leader>x', '<cmd>source %<CR>', { desc = '[Run] current lua flie' })
-vim.keymap.set('n', '<C-R>', ':.lua<CR>', { desc = '[Run] current lua line' })
+vim.keymap.set('n', '<C-R>', '<cmd>source %<CR>', { desc = '[Run] current lua flie' })
 vim.keymap.set('v', '<C-R>', ':lua<CR>', { desc = '[Run] selected lua area' })
